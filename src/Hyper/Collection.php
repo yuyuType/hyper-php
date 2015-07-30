f0a6da8b930046e71cbef293b2fad2309ac054e9
yuyuType <yu_yu81@hotmail.com>
2015-07-30 18:07:36 +0900

    add list functions

--- src/Hyper/Collection.php
+++ src/Hyper/Collection.php
@@ -188,7 +188,7 @@ class Collection
      */
     public static function reverse($iterator)
     {
-        return array_reverse(iterator_to_array(self::toIterator($iterator)));
+        return new \ArrayIterator(array_reverse(iterator_to_array(self::toIterator($iterator))));
     }
 
     /**
@@ -220,6 +220,594 @@ class Collection
             if ($it->valid()) {
                 yield $sep;
             }
+        }
+    }
+
+    /**
+     * intercalate
+     *
+     * ex)
+     *
+     * ``` php
+     * $arr = [[3, 4], [5, 6], [7, 8]];
+     * $value = Collection::intercalate([1, 2], $arr); // [3, 4, 1, 2, 5, 6, 1, 2, 7, 8]
+     * ```
+     *
+     * @param array|Traversable $xs
+     * @param array|Traversable $xss
+     * @return Generator
+     * @SuppressWarnings(PHPMD.ShortVariable)
+     */
+    public static function intercalate($xs, $xss)
+    {
+        $it = self::toIterator($xss);
+        while ($it->valid()) {
+            foreach ($it->current() as $value) {
+                yield $value;
+            }
+            $it->next();
+            if ($it->valid()) {
+                foreach ($xs as $x) {
+                    yield $x;
+                }
+            }
+        }
+    }
+
+    /**
+     * transpose
+     *
+     * ex)
+     *
+     * ``` php
+     * $arr = [[1, 2], [3, 4], [5, 6], [7, 8]];
+     * $value = Collection::transpose($arr); // [[1, 3, 5, 7], [2, 4, 6, 8]]
+     * ```
+     *
+     * @param array|Traversable $iterator
+     * @return ArrayIterator
+     * @SuppressWarnings(PHPMD.ShortVariable)
+     */
+    public static function transpose($iterator)
+    {
+        $it = self::toIterator($iterator);
+        $result = [];
+        foreach ($it as $xs) {
+            $n = 0;
+            foreach ($xs as $x) {
+                $result[$n++][] = $x;
+            }
+        }
+        return new \ArrayIterator($result);
+    }
+
+    /**
+     * subsequences
+     *
+     * ex)
+     *
+     * ``` php
+     * ```
+     *
+     * @param array|Traversable $xs
+     * @return Generator
+     * @SuppressWarnings(PHPMD.ShortVariable)
+     */
+    public static function subsequences($xs)
+    {
+        // todo@not implimented
+    }
+
+    /**
+     * permutations
+     *
+     * ex)
+     *
+     * ``` php
+     * ```
+     *
+     * @param array|Traversable $xss
+     * @return Generator
+     * @SuppressWarnings(PHPMD.ShortVariable)
+     */
+    public static function permutations($xs)
+    {
+        // todo@not implimented
+    }
+
+    // Reducing lists
+
+    /**
+     * fold
+     *
+     * ex)
+     *
+     * ``` php
+     * ```
+     *
+     * @param callable $f
+     * @param mixed $z
+     * @param array|Traversable $xs
+     * @return $mixed
+     * @SuppressWarnings(PHPMD.ShortVariable)
+     */
+    public static function fold(callable $f, $z, $xs)
+    {
+        foreach ($xs as $x) {
+            $z = call_user_func($f, $z, $x);
+        }
+        return $z;
+    }
+
+    /**
+     * fold1
+     *
+     * ex)
+     *
+     * ``` php
+     * ```
+     *
+     * @param callable $f
+     * @param array|Traversable $xs
+     * @return None|Some
+     * @SuppressWarnings(PHPMD.ShortVariable)
+     */
+    public static function fold1(callable $f, $xs)
+    {
+        return self::uncons($xs)->map(function ($ys) use ($f) {
+            $z = $ys[0];
+            foreach ($ys[1] as $y) {
+                $z = call_user_func($f, $z, $y);
+            }
+            return $z;
+        });
+    }
+
+    // Special folds
+
+    /**
+     * concat
+     *
+     * ex)
+     *
+     * ``` php
+     * ```
+     *
+     * @param array|Traversable $xss
+     * @SuppressWarnings(PHPMD.ShortVariable)
+     */
+    public static function concat($xss)
+    {
+        foreach ($xss as $xs) {
+            foreach ($xs as $x) {
+                yield $x;
+            }
+        }
+    }
+
+    /**
+     * concatMap
+     *
+     * ex)
+     *
+     * ``` php
+     * ```
+     *
+     * @param callable $f
+     * @param array|Traversable $xss
+     * @SuppressWarnings(PHPMD.ShortVariable)
+     */
+    public static function concatMap(callable $f, $xss)
+    {
+        return self::concat(self::map($f, $xss));
+    }
+
+    /**
+     * any
+     *
+     * ex)
+     *
+     * ``` php
+     * ```
+     *
+     * @param callable $pred
+     * @param array|Traversable $xs
+     * @SuppressWarnings(PHPMD.ShortVariable)
+     */
+    public static function any(callable $pred, $xs)
+    {
+        foreach ($xs as $x) {
+            if (call_user_func($pred, $x)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * all
+     *
+     * ex)
+     *
+     * ``` php
+     * ```
+     *
+     * @param callable $pred
+     * @param array|Traversable $xs
+     * @SuppressWarnings(PHPMD.ShortVariable)
+     */
+    public static function all(callable $pred, $xs)
+    {
+        $ret = true;
+        foreach ($xs as $x) {
+            $ret = call_user_func($pred, $x) && $ret;
+        }
+        return $ret;
+    }
+
+    /**
+     * sum
+     *
+     * ex)
+     *
+     * ``` php
+     * ```
+     *
+     * @param array|Traversable $xs
+     * @SuppressWarnings(PHPMD.ShortVariable)
+     */
+    public static function sum($xs)
+    {
+        return self::fold(function ($z, $x) { return $z + $x; }, 0, $xs);
+    }
+
+    /**
+     * product
+     *
+     * ex)
+     *
+     * ``` php
+     * ```
+     *
+     * @param array|Traversable $xs
+     * @SuppressWarnings(PHPMD.ShortVariable)
+     */
+    public static function product($xs)
+    {
+        return self::fold(function ($z, $x) { return $z * $x; }, 1, $xs);
+    }
+
+    /**
+     * maximum
+     *
+     * ex)
+     *
+     * ``` php
+     * ```
+     *
+     * @param array|Traversable $xs
+     * @SuppressWarnings(PHPMD.ShortVariable)
+     */
+    public static function maximum($xs)
+    {
+        foreach ($xs as $x) {
+            if (!isset($max)) {
+                $max = $x;
+            }
+            $max = max($max, $x);
+        }
+        return !isset($max)
+            ? Option::None()
+            : Option::Some($max);
+    }
+
+    /**
+     * maximumBy
+     *
+     * ex)
+     *
+     * ``` php
+     * ```
+     *
+     * @param callable $cmp
+     * @param array|Traversable $xs
+     * @SuppressWarnings(PHPMD.ShortVariable)
+     */
+    public static function maximumBy(callable $cmp, $xs)
+    {
+        foreach ($xs as $x) {
+            if (!isset($max)) {
+                $max = $x;
+            }
+            $max = call_user_func($cmp, $max, $x) === true
+                ? $max
+                : $x;
+        }
+        return !isset($max)
+            ? Option::None()
+            : Option::Some($max);
+    }
+
+    /**
+     * minimum
+     *
+     * ex)
+     *
+     * ``` php
+     * ```
+     *
+     * @param array|Traversable $xs
+     * @SuppressWarnings(PHPMD.ShortVariable)
+     */
+    public static function minimum($xs)
+    {
+        foreach ($xs as $x) {
+            if (!isset($min)) {
+                $min = $x;
+            }
+            $min = min($min, $x);
+        }
+        return !isset($min)
+            ? Option::None()
+            : Option::Some($min);
+    }
+
+    /**
+     * minimumBy
+     *
+     * ex)
+     *
+     * ``` php
+     * ```
+     *
+     * @param callable $cmp
+     * @param array|Traversable $xs
+     * @SuppressWarnings(PHPMD.ShortVariable)
+     */
+    public static function minimumBy(callable $cmp, $xs)
+    {
+        foreach ($xs as $x) {
+            if (!isset($min)) {
+                $min = $x;
+            }
+            $min = call_user_func($cmp, $min, $x) === true
+                ? $x
+                : $min;
+        }
+        return !isset($min)
+            ? Option::None()
+            : Option::Some($min);
+    }
+
+    // Scans
+
+    /**
+     * scan
+     *
+     * ex)
+     *
+     * ``` php
+     * ```
+     *
+     * @param callable $f
+     * @param mixed $q
+     * @param array|Traversable $xs
+     * @SuppressWarnings(PHPMD.ShortVariable)
+     */
+    public static function scan(callable $f, $q, $xs)
+    {
+        yield $q;
+        foreach (self::toIterator($xs) as $x) {
+            $q = call_user_func($f, $q, $x);
+            yield $q;
+        }
+    }
+
+    /**
+     * scan1
+     *
+     * ex)
+     *
+     * ``` php
+     * ```
+     *
+     * @param callable $f
+     * @param array|Traversable $xs
+     * @SuppressWarnings(PHPMD.ShortVariable)
+     */
+    public static function scan1(callable $f, $xs)
+    {
+        return self::uncons($xs)->map(function ($ys) use ($f) {
+            $q = $ys[0];
+            yield $q;
+            foreach ($ys[1] as $y) {
+                $q = call_user_func($f, $q, $y);
+                yield $q;
+            }
+        });
+    }
+
+    // Accumulating maps
+
+    /**
+     * mapAccum
+     *
+     * ex)
+     *
+     * ``` php
+     * ```
+     *
+     * @param callable $f
+     * @param mixed $z
+     * @param array|Traversable $xs
+     * @SuppressWarnings(PHPMD.ShortVariable)
+     */
+    public static function mapAccum(callable $f, $z, $xs)
+    {
+        $result = [];
+        foreach ($xs as $x) {
+            $q = call_user_func($f, $z, $x);
+            $z = $q[0];
+            $result[] = $q[1];
+        }
+        return [$z, $result];
+    }
+
+    // Infinite lists
+
+    /**
+     * iterate
+     *
+     * ex)
+     *
+     * ``` php
+     * ```
+     *
+     * @param callable $f
+     * @param mixed $z
+     * @param array|Traversable $xs
+     * @SuppressWarnings(PHPMD.ShortVariable)
+     */
+    public static function iterate(callable $f, $x)
+    {
+        $z = $x;
+        yield $z;
+        while (true) {
+            $z = call_user_func($f, $z);
+            yield $z;
+        }
+    }
+
+    /**
+     * repeat
+     *
+     * ex)
+     *
+     * ``` php
+     * ```
+     *
+     * @param mixed $x
+     * @return Generator
+     * @SuppressWarnings(PHPMD.ShortVariable)
+     */
+    public static function repeat($x)
+    {
+        while (true) {
+            yield $x;
+        }
+    }
+
+    /**
+     * replicate
+     *
+     * ex)
+     *
+     * ``` php
+     * ```
+     *
+     * @param mixed $x
+     * @return Generator
+     * @SuppressWarnings(PHPMD.ShortVariable)
+     */
+    public static function replicate($n, $x)
+    {
+        return self::take($n, self::repeat($x));
+    }
+
+    /**
+     * cycle
+     *
+     * ex)
+     *
+     * ``` php
+     * ```
+     *
+     * @param array|Traversable $xs
+     * @return Generator
+     * @SuppressWarnings(PHPMD.ShortVariable)
+     */
+    public static function cycle($xs)
+    {
+        while (true) {
+            foreach ($xs as $k => $v) {
+                yield $k => $v;
+            }
+        }
+    }
+
+    // Unfolding
+
+    /**
+     * unfold
+     *
+     * ex)
+     *
+     * ``` php
+     * ```
+     *
+     * @param callable $f
+     * @param mixed $b
+     * @return Generator
+     * @SuppressWarnings(PHPMD.ShortVariable)
+     */
+    public static function unfold($f, $b)
+    {
+        while (true) {
+            $value = call_user_func($f, $b);
+            if ($value->isEmpty) {
+                break;
+            }
+            $result = $value->getOrElse(null);
+            yield $result[0];
+            $b = $result[1];
+        }
+    }
+
+    // Extracting sublists
+
+    /**
+     * take
+     *
+     * ex)
+     *
+     * ``` php
+     * ```
+     *
+     * @param integer $n
+     * @param array|Traversable $xs
+     * @retun Generator
+     * @SuppressWarnings(PHPMD.ShortVariable)
+     */
+    public static function take($n, $xs)
+    {
+        foreach (self::toIterator($xs) as $x) {
+            if ($n-- <= 0) {
+                break;
+            }
+            yield $x;
+        }
+    }
+
+    /**
+     * drop
+     *
+     * ex)
+     *
+     * ``` php
+     * ```
+     *
+     * @param integer $n
+     * @param array|Traversable $xs
+     * @retun Generator
+     * @SuppressWarnings(PHPMD.ShortVariable)
+     */
+    public static function drop($n, $xs)
+    {
+        foreach (self::toIterator($xs) as $x) {
+            if ($n-- > 0) {
+                continue;
+            }
+            yield $x;
         }
     }
 
--- tests/CollectionTest.php
+++ tests/CollectionTest.php
@@ -70,9 +70,9 @@ class CollectionTest extends PHPUnit_Framework_TestCase
     public function testReverse()
     {
         $arr = ['val' => 1, 'val1' => 2, 'val2' => 3];
-        $this->assertSame(['val2' => 3, 'val1' => 2, 'val' => 1], Collection::reverse($arr));
-        $this->assertSame(['val2' => 3, 'val1' => 2, 'val' => 1], Collection::reverse(new ArrayObject($arr)));
-        $this->assertSame([], Collection::reverse([]));
+        $this->assertSame(['val2' => 3, 'val1' => 2, 'val' => 1], iterator_to_array(Collection::reverse($arr)));
+        $this->assertSame(['val2' => 3, 'val1' => 2, 'val' => 1], iterator_to_array(Collection::reverse(new ArrayObject($arr))));
+        $this->assertSame([], iterator_to_array(Collection::reverse([])));
     }
 
     public function testIntersperse()
@@ -82,6 +82,170 @@ class CollectionTest extends PHPUnit_Framework_TestCase
         $this->assertSame([], iterator_to_array(Collection::intersperse(',', [])));
         $this->assertsame("t,e,s,t",  join(iterator_to_array(Collection::intersperse(',', "test"))));
         $this->assertSame(["Tom", ",", "Jhon"], iterator_to_array(Collection::intersperse(',', ["Tom", "Jhon"])));
+    }
+
+    public function testIntercalate()
+    {
+        $arr = [[3, 4], [5, 6], [7, 8]];
+        $value = Collection::intercalate([1, 2], $arr);
+        $this->assertSame([3, 4, 1, 2, 5, 6, 1, 2, 7, 8], iterator_to_array($value));
+    }
+
+    public function testTranspose()
+    {
+        $arr = [[1, 2], [3, 4], [5, 6], [7, 8]];
+        $value = Collection::transpose($arr); // [[1, 3, 5, 7], [2, 4, 6, 8]]
+        $this->assertSame([[1, 3, 5, 7], [2, 4, 6, 8]], iterator_to_array($value));
+
+        $arr = [[10, 11], [20], [], [30, 31, 32]];
+        $value = Collection::transpose($arr);
+        $this->assertSame([[10, 20, 30], [11, 31], [32]], iterator_to_array($value));
+    }
+
+    public function testFold()
+    {
+        $value = Collection::fold(function ($z, $x) { return $z + $x; }, 0, range(1, 10));
+        $this->assertSame(55, $value);
+    }
+
+    public function testFold1()
+    {
+        $value = Collection::fold1(function ($z, $x) { return $z + $x; }, range(1, 10));
+        $this->assertTrue($value->exists(function ($x) { return $x === 55; }));
+    }
+
+    public function testConcat()
+    {
+        $value = Collection::concat([[1, 2, 3], [4, 5, 6]]);
+        $this->assertSame([1, 2, 3, 4, 5, 6], iterator_to_array($value));
+    }
+
+    public function testConcatMap()
+    {
+        $value = Collection::concatMap(function ($xs) { return [$xs[0]]; }, [[1, 2, 3], [4, 5, 6]]);
+        $this->assertSame([1, 4], iterator_to_array($value));
+    }
+
+    public function testAny()
+    {
+        $this->assertTrue(Collection::any(function ($x) { return $x === 2; }, [1, 2, 3]));
+        $this->assertTrue(Collection::any(function ($x) { return $x === 3; }, [1, 2, 3]));
+        $this->assertFalse(Collection::any(function ($x) { return $x === 4; }, [1, 2, 3]));
+    }
+
+    public function testAll()
+    {
+        $this->assertTrue(Collection::all(function ($x) { return $x === 2; }, [2, 2, 2]));
+        $this->assertTrue(Collection::all(function ($x) { return $x === 3; }, [3, 3, 3]));
+        $this->assertFalse(Collection::all(function ($x) { return $x === 4; }, [1, 2, 3]));
+        $this->assertFalse(Collection::all(function ($x) { return $x === 2; }, [1, 2, 3]));
+    }
+
+    public function testMaximum()
+    {
+        $this->assertEquals(Option::Some(3), Collection::maximum([1, 2, 3]));
+        $this->assertEquals(Option::None(), Collection::maximum([]));
+    }
+
+    public function testMaximumBy()
+    {
+        $this->assertEquals(Option::Some("asdfas"), Collection::maximumBy(function ($x, $y) {
+            return mb_strlen($x) > mb_strlen($y);
+        }, ["abc", "asdfaf", "sd", "sddf", "asdfas"]));
+        $this->assertEquals(Option::None(), Collection::maximumBy(function () {}, []));
+    }
+
+    public function testMinimum()
+    {
+        $this->assertEquals(Option::Some(1), Collection::minimum([1, 2, 3]));
+        $this->assertEquals(Option::None(), Collection::minimum([]));
+    }
+
+    public function testMinimumBy()
+    {
+        $this->assertEquals(Option::Some("sd"), Collection::minimumBy(function ($x, $y) {
+            return mb_strlen($x) > mb_strlen($y);
+        }, ["abc", "asdfaf", "sd", "sddf", "asdfas", "dd"]));
+        $this->assertEquals(Option::None(), Collection::minimumBy(function () {}, []));
+    }
+
+    public function testSum()
+    {
+        $this->assertSame(55, Collection::sum(range(1, 10)));
+    }
+
+    public function testScan()
+    {
+        $this->assertSame([1,1,2,6,24,120,720,5040,40320,362880,3628800], iterator_to_array(Collection::scan(function ($x, $y) { return $x * $y; }, 1, range(1, 10))));
+    }
+
+    public function testScan1()
+    {
+        $this->assertTrue(Collection::scan1(function ($x, $y) { return $x * $y; }, range(1, 10))
+            ->exists(function ($xs) { return [1,2,6,24,120,720,5040,40320,362880,3628800] === iterator_to_array($xs); }));
+    }
+
+    public function testMapAccum()
+    {
+        $value = Collection::mapAccum(function ($x, $y) { return [$x, $x * $y]; }, 1, range(1, 10));
+        $this->assertSame([1, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]], $value);
+
+        $value = Collection::mapAccum(function ($x, $y) { return [$x + $y, $x * $y]; }, 1, range(1, 10));
+        $this->assertSame([56, [1, 4, 12, 28, 55, 96, 154, 232, 333, 460]], $value);
+    }
+
+    public function testIterate()
+    {
+        $value = Collection::take(5, Collection::iterate(function ($x) { return $x + $x; }, 1));
+        $this->assertSame([1, 2, 4, 8, 16], iterator_to_array($value));
+    }
+
+    public function testRepeat()
+    {
+        $value = Collection::take(5, Collection::repeat(1));
+        $this->assertSame([1, 1, 1, 1, 1], iterator_to_array($value));
+    }
+
+    public function testReplicate()
+    {
+        $value = Collection::replicate(5, 1);
+        $this->assertSame([1, 1, 1, 1, 1], iterator_to_array($value));
+    }
+
+    public function testCycle()
+    {
+        $value = Collection::take(5, Collection::cycle([1, 2]));
+        $this->assertSame([1, 2, 1, 2, 1], iterator_to_array($value));
+    }
+
+    public function testUnfold()
+    {
+        $value = Collection::unfold(function ($x) {
+            if ($x > 10) {
+                return Option::None();
+            }
+            return Option::Some([$x, $x * 2]);
+        }, 1);
+        $this->assertSame([1, 2, 4, 8], iterator_to_array($value));
+
+        $value = Collection::unfold(function ($pair) {
+            $a = $pair[0];
+            $b = $pair[1];
+            return Option::Some([$a + $b, [$b, $b + $a]]);
+        }, [0, 1]);
+        $this->assertSame([1,2,3,5,8,13,21,34,55,89], iterator_to_array(Collection::take(10, $value)));
+    }
+
+    public function testTake()
+    {
+        $value = Collection::take(5, [1, 2, 3, 4, 5, 6]);
+        $this->assertSame([1, 2, 3, 4, 5], iterator_to_array($value));
+    }
+
+    public function testDrop()
+    {
+        $value = Collection::drop(3, [1, 2, 3, 4, 5]);
+        $this->assertSame([4, 5], iterator_to_array($value));
     }
 
     public function testLookup()
